86ce38f3ca32052a911f60ff506114e652b5e93e
Kausar Ahmed <111121885+kausaraahmed@users.noreply.github.com>
2022-10-23 12:28:41 +0600

    Delete Lab file - 01 23.10.22 directory

--- Lab file - 01 23.10.22/take_input_from_user.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int input_1, input_2;
-    printf("Enter two numbers: \n");
-    scanf("%d %d", &input_1, &input_2);
-    printf("You entered %d and %d as input.", input_1, input_2);
-}
