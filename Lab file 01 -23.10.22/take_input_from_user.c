e7b0aba23b51f3c402f7098525c11a94c0ce6114
Kausar Ahmed <111121885+kausaraahmed@users.noreply.github.com>
2022-10-23 12:25:46 +0600

    Add files via upload

---
+++ Lab file 01 -23.10.22/take_input_from_user.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int input_1, input_2;
+    printf("Enter two numbers: \n");
+    scanf("%d %d", &input_1, &input_2);
+    printf("You entered %d and %d as input.", input_1, input_2);
+}
---
+++ Lab file 01 -23.10.22/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int _int = 4;
+    char _char = 'K';
+    float _float = 3.1416;
+    char _string[] = {"Kausar Ahmed"};
+
+    printf("\n\nThis is Integer type data: %d", _int);
+    printf("\n\nThis is Character type data: %c", _char);
+    printf("\n\nThis is float type data: %f", _float);
+    printf("\n\nThis is String type data: %s", _string);
+}
